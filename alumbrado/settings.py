fb8e097a19de5bc9f8b5e1371aaa6b933e8ccadd
W-Varg <kryshot05@gmail.com>
2018-12-18 23:42:20 -0400

    3ro desplegamiento

--- alumbrado/settings.py
+++ alumbrado/settings.py
@@ -128,3 +128,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
