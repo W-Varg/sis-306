9381679739ae0f9930f3737da86ff59ff128cc94
W-Varg <kryshot05@gmail.com>
2018-12-19 02:10:41 -0400

    3ro desplegamiento

--- a_publico/views/__init__.py
+++ a_publico/views/__init__.py
@@ -18,11 +18,12 @@ def index(request):
     return render(request, 'index.html',context)
 
 def inf_mando(request):
-    list_r = CuadroMando.objects.prefetch_related('codigocalle__barriopedania')
+    list_r = CuadroMando.objects.select_related('codigocalle','codigobarrio','codigocalle__codigotipovia').order_by('codigobarrio__nombrebarrio')
     # list_total = Registry.objects.count()
     context = {'list_cuadro' : list_r}
     return render(request, 'inf_cuadro.html',context)
 
 def inf_lampara(request):
-    context = {'lamparas' : 'lam'}
+    lam_r = PuntoLuz.objects.select_related('codigocalle','codigobarrio', 'codigoluminaria','codigoluminaria__codigolampara','codigocalle__codigotipovia','codigocuadro').order_by('codigocuadro__codigoindentificacion')
+    context = {'lamparas' : lam_r}
     return render(request,'inf_lampara.html',context)
